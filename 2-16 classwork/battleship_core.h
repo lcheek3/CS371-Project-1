c2f5193709c5f1e4562a75edf43094a7c8ff31c8
Nick Segler <njsegler@alaska.edu>
2016-02-19 16:31:27 -0900

    add ships

--- 2-16 classwork/battleship_core.h
+++ 2-16 classwork/battleship_core.h
@@ -2,16 +2,68 @@
 #define BATTLESHIP_CORE_H_INCLUDED
 
 #include <iostream>
+#include <vector>
 
 int runGame() {
 	std::cout << "Welcome to Battleship!\n";
 	return 0;
 }
 
+class Ship {
+private:
+    int pos_x;
+    int pos_y;
+    int direction;
+    int length;
+public:
+    Ship():pos_x(0),pos_y(0),direction(0),length(1)
+    {}
+    
+    Ship(int x, int y, int d, int l):pos_x(x),pos_y(y),direction(d),length(l)
+    {}
+    
+    std::pair<int,int> operator[] (int indx)
+    {
+        if(indx>=0 && indx<length)
+        {
+            if(direction==0)
+                return std::make_pair(pos_x, pos_y+indx);
+        
+            if(direction==1)
+                return std::make_pair(pos_x+indx, pos_y);
+        }
+        
+        return std::make_pair(-1,-1);
+    }
+    
+    bool operator()(int x, int y)
+    {
+        bool hit = false;
+        for(int i=0; i<length; ++i)
+        {
+            if(direction==0)
+            {
+                if(x==pos_x && y==pos_y+i)
+                {
+                    hit=true;
+                }
+            }else if(direction==1)
+            {
+                if(x==pos_x+i && y==pos_y)
+                {
+                    hit=true;
+                }
+            }
+        }
+        return hit;
+    }
+};
+
 class Board {
 private:
 	int board_x;
 	int board_y;
+    std::vector<Ship> boats;
 public:
 	Board() {
 		board_x = 0;
@@ -27,6 +79,26 @@ public:
 	int getY() {
 		return board_y;
 	}
+    
+    void placeShip(int x, int y, int d, int l)
+    {
+        Ship temp(x,y,d,l);
+        boats.push_back(temp);
+    }
+    
+    std::vector<Ship> getShips()
+    {
+        return boats;
+    }
+    
+    bool checkShot(int x, int y)
+    {
+        if(boats[0](x,y))
+            return true;
+        return false;
+    }
 };
+
+
 
 #endif
--- 2-16 classwork/battleship_test.cpp
+++ 2-16 classwork/battleship_test.cpp
@@ -2,7 +2,7 @@
 #include "catch.hpp"
 #include "battleship_core.h"
 #include <iostream>
-
+#include <vector>
 
 
 TEST_CASE("test case of TEST_CASE") {
@@ -16,4 +16,81 @@ TEST_CASE("Test Case: Board Bound") {
 	REQUIRE(testBoard.getX() == 10);
 
 	REQUIRE(testBoard.getY() == 10);
+}
+
+TEST_CASE("Test Case: Ship creation and placement") {
+    
+    std::vector<int> t_x;
+    std::vector<int> t_y;
+    std::vector<int> t_d;
+    std::vector<int> t_l;
+    
+    t_x.push_back(0);
+    t_y.push_back(0);
+    t_d.push_back(0);
+    t_l.push_back(3);
+    
+    t_x.push_back(4);
+    t_y.push_back(4);
+    t_d.push_back(1);
+    t_l.push_back(4);
+    
+    t_x.push_back(2);
+    t_y.push_back(4);
+    t_d.push_back(0);
+    t_l.push_back(5);
+    
+    Board testBoard;
+    std::vector<Ship> testShips;
+    
+    for(int i=0; i<t_x.size(); ++i)
+        testBoard.placeShip(t_x[i], t_y[i], t_d[i], t_l[i]);
+    
+    testShips = testBoard.getShips();
+    
+    REQUIRE(testShips[0][0]==std::make_pair(0, 0));
+    REQUIRE(testShips[0][1]==std::make_pair(0, 1));
+    REQUIRE(testShips[0][2]==std::make_pair(0, 2));
+    REQUIRE(testShips[0][3]==std::make_pair(-1, -1));
+    
+    REQUIRE(testShips[1][0]==std::make_pair(4, 4));
+    REQUIRE(testShips[1][1]==std::make_pair(5, 4));
+    REQUIRE(testShips[1][2]==std::make_pair(6, 4));
+    REQUIRE(testShips[1][3]==std::make_pair(7, 4));
+    REQUIRE(testShips[1][4]==std::make_pair(-1,-1));
+    
+    REQUIRE(testShips[2][0]==std::make_pair(2, 4));
+    REQUIRE(testShips[2][1]==std::make_pair(2, 5));
+    REQUIRE(testShips[2][2]==std::make_pair(2, 6));
+    REQUIRE(testShips[2][3]==std::make_pair(2, 7));
+    REQUIRE(testShips[2][4]==std::make_pair(2, 8));
+    REQUIRE(testShips[2][5]==std::make_pair(-1,-1));
+    
+}
+
+TEST_CASE("Test Case: Check a fired shot"){
+    std::vector<int> t_x;
+    std::vector<int> t_y;
+    std::vector<int> t_d;
+    std::vector<int> t_l;
+    
+    t_x.push_back(0);
+    t_y.push_back(0);
+    t_d.push_back(0);
+    t_l.push_back(3);
+    
+    Board testBoard;
+    
+    for(int i=0; i<t_x.size(); ++i)
+        testBoard.placeShip(t_x[i], t_y[i], t_d[i], t_l[i]);
+    
+    REQUIRE(testBoard.checkShot(t_x[0], t_y[0]-1)==false);
+    REQUIRE(testBoard.checkShot(t_x[0], t_y[0])==true);
+    REQUIRE(testBoard.checkShot(t_x[0], t_y[0]+1)==true);
+    REQUIRE(testBoard.checkShot(t_x[0], t_y[0]+2)==true);
+    REQUIRE(testBoard.checkShot(t_x[0], t_y[0]+3)==false);
+    REQUIRE(testBoard.checkShot(t_x[0]+1, t_y[0])==false);
+    
+
+
 }
