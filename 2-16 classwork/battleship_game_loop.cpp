5159689c83d40ebf706bbf08c327fec2a5ca2135
Nick Segler <njsegler@alaska.edu>
2016-02-26 15:17:51 -0900

    fixing master branch

--- 2-16 classwork/battleship_game_loop.cpp
+++ 2-16 classwork/battleship_game_loop.cpp
@@ -1,8 +1,10 @@
 #include "battleship_core.h"
+#include <string>
 #include <iostream>
 #include <vector>
 
 int main() {
+    std::string name;
 	Board player1(10, 10,"Player1");
 	Board player2(10, 10,"Player2");
     player1.nameSetup();
@@ -15,18 +17,19 @@ int main() {
 		player1.turn(player2);
 		if (player2.lost())
         {
-            std::cout <<player1.getName() <<" Wins!" <<std::endl;
+            std::cout <<"\n\n\n\n\n\n\n\n\n" << player1.getName() << ": You have won! Congraduations" <<std::endl;
+            name = player1.getName();
             break;
         }
         std::cout <<player1.getName() <<" pass to " <<player2.getName() <<std::endl;
         player2.turn(player1);
         if (player1.lost())
         {
-            std::cout <<player2.getName() <<" Wins!" <<std::endl;
+            std::cout <<"\n\n\n\n\n\n\n\n\n" << player2.getName() << ": You have won! Congraduations" <<std::endl;
+            name = player2.getName();
             break;
         }
-        
 	}
-
+    
 	return 0;
 }
